0b5569dcb976b2c6b8603b1f0922620fe293d02a
João Batista <jbsouzasantos1999@gmail.com>
2022-05-12 22:11:31 -0300

    May need some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import classes from "./App.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("loading...");
+  const [gender, setGender] = useState("loading...");
+  const [status, setStatus] = useState("loading...");
+  const [species, setSpecies] = useState("loading...");
+  const [origin, setOrigin] = useState("loading...");
+  const [location, setLocation] = useState("loading...");
+  const [image, setImage] = useState();
+
+  const [charNumber, setCharNumber] = useState(1);
+
+  useEffect(() => {
+    handleClick();
+  }, []);
+
+  const fetchData = () => {
+    fetch(`https://rickandmortyapi.com/api/character/${charNumber}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setName(data.name);
+        setGender(data.gender);
+        setStatus(data.status);
+        setSpecies(data.species);
+        setOrigin(data.origin.name);
+        setLocation(data.location.name);
+        setImage(data.image);
+      });
+  };
+
+  const handleClick = () => {
+    setCharNumber(Math.floor(Math.random() * (826 - 1)) + 1);
+    fetchData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+      <div className={classes.portrait}>
+        <img src={image} alt={name} className={classes.image} />
+      </div>
+      <div className={classes.information}>
+        <main className={classes.content}>
+          <h2 className={classes.name}>{name}</h2>
+          <section className={classes.stats}>
+            <div className={classes.list}>
+              <span>Gender: {gender}</span>
+              <span>Status: {status}</span>
+              <span>Species: {species}</span>
+              <span>Origin: {origin}</span>
+              <span>Location: {location}</span>
+            </div>
+          </section>
+        </main>
+      </div>
+      <div className={classes["button-container"]}>
+        <button className={classes.change}>
+          <span className={classes["button-text"]} onClick={handleClick}>
+            Change Charcter
+          </span>
+          <FontAwesomeIcon icon={faArrowsRotate} />
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
